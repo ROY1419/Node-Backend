569a3aa786b79d92eb7ec5f423439f0b2d24f6fb
Roy1419 <royshubham471@gmail.com>
2024-11-26 13:14:36 +0530

    this apiResponse.js is modified for seame less experence

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,7 @@
 class ApiResponse {
     constructor(statuscode, data, message = "Success"){
         this.statuscode = statuscode
-        this.data = null
+        this.data = data
         this.message = message
         this.success = statuscode < 400
     }
