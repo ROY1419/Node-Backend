6bb6a49b218ed6612a6c3cccbc216a55c9bb142c
Roy1419 <royshubham471@gmail.com>
2024-11-26 13:17:23 +0530

    this is user controller modified for users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -41,14 +41,16 @@ const registerUser = asyncHandler(async (req, res) => {
     // check if user already exists: username, email
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.d) && req.files.d.length > 0) {
-        coverImageLocalPath = req.files.d[0].path
-    }
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+
+    // if (req.files && Array.isArray(req.files.d) && req.files.d.length > 0) {
+    //     coverImageLocalPath = req.files.d[0].path
+    // }
     // check for images, check for avatar
 
     if (!avatarLocalPath) {
-        return new ApiError(400, "Avatar file is required")
+        throw new ApiError(400, "Avatar file is required")
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
@@ -57,7 +59,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     // upload them to cloudinary, avatar
 
-    await User.create({
+    const user = await User.create({
         fullname,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
