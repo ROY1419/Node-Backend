7ddd547581e7edd5beb9e6482b81760ae67b1141
Roy1419 <royshubham471@gmail.com>
2025-01-11 22:56:57 +0530

    this commit is for modification of auth middlware file.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -13,7 +13,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-        const user = await User.findById(decoden?._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
 
         if (!User) {
             throw new ApiError(401, "Invaild Access Token")
