b44eee62f1cec56b5f9f95b28ec22504d2f9f405
Roy1419 <royshubham471@gmail.com>
2025-01-12 20:59:32 +0530

    this commit is for modification of index file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from './app.js'
+
+
 dotenv.config({
-    path:'./.env'
+    path: './.env'
 })
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT, ()=> {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-}).catch((err) => {
-    console.log("MONGODB connection failed !!! ", err);
-});
-
-
-
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
+    .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
         })
+    }).catch((err) => {
+        console.log("MONGODB connection failed !!! ", err);
+    });
+// import express from "express"
+// const app = express()
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror", (error) => {
+//             console.log("ERRR: ", error);
+//             throw error
+//         })
 
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-*/
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
