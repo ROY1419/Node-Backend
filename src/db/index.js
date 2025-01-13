f0f07ff9c93ed1f99a91cb1594c9998e62e6a197
Roy1419 <royshubham471@gmail.com>
2025-01-13 22:20:02 +0530

    this commit is for modification of index.js file on db folder

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n Mongodb Connected !! DB Host: ${ConnectionInstance.connection.host}`);
+        console.log(`\n Mongodb Connected !! DB Host: ${ConnectionInstance.connection.name}`);
 
         
     } catch (error) {
